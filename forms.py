34b0a9cccfd810b4c0a89ff50b8321342612bbd2
KesterDaniel <kesterdan17@gmail.com>
2023-03-27 16:58:19 +0100

    adding improvements

--- forms.py
+++ forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField)
+from wtforms import (StringField, PasswordField, SubmitField)
 from wtforms.validators import InputRequired, Length
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(),
-                                             Length(max=30)])
-    password = StringField("Password", validators=[InputRequired(),
-                                                   Length(max=30)])
+    email = StringField('Email', validators=[InputRequired(), Length(max=30)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(max=30)])
+    submit = SubmitField("Submit")
