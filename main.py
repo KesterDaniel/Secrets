27bf7b2cbe6dd6ebc14b4b0faabf9996bd7c7741
KesterDaniel <kesterdan17@gmail.com>
2023-03-27 16:45:53 +0100

    displaying the web forms

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from forms import LoginForm
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("MY_SECRET_KEY")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
