7a4d27c05e6c5c97cccac1845db2f857d3036b6d
KesterDaniel <kesterdan17@gmail.com>
2023-03-27 17:22:26 +0100

    validating email input

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField)
-from wtforms.validators import InputRequired, Length
+from wtforms import (StringField, PasswordField, SubmitField, EmailField)
+from wtforms.validators import InputRequired, Length, Email
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Length(max=30)])
-    password = PasswordField("Password", validators=[InputRequired(), Length(max=30)])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired(), Length(max=8)])
     submit = SubmitField("Submit")
